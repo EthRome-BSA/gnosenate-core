804b52173c37767713ea3d445749feb75420ddb8
TheoLeFur <theo.lefur@epfl.ch>
2023-10-07 17:08:35 +0200

    Inbox

--- src/Inbox.sol
+++ src/Inbox.sol
@@ -5,22 +5,31 @@ import {IInbox} from "src/interfaces/IInbox.sol";
 
 contract Inbox is IInbox {
     
-    event RequestEmited(bytes name);
-    event ReviewEmited(bytes review);
+    event RequestEmited(bytes nameEncoded);
+    event ProtocolRegistered(bytes nameEncoded);
+    event ReviewEmited(bytes reviewEncoded);
 
     error NotAContract(address _address);
-    mapping (address => string) contractToAppName;
+    mapping (address => bytes) contractToAppName;
+    mapping (address => mapping(address => uint256)) userToContractVisitationFreq;
 
-    function emitRequest(string memory name) external {
+    function registerContract(string calldata name) external {
         if (isContract(msg.sender)) {
-            contractToAppName[msg.sender] = name;
-            emit RequestEmited(abi.encode(name));
+            contractToAppName[msg.sender] = abi.encode(name);
+            emit ProtocolRegistered(abi.encode(name));
         } else {
             revert NotAContract(msg.sender); 
         }
     }
-    
-    function emitReview(string memory review) external {
+
+    function emitRequest(string calldata name) external returns (address, uint256) {
+        uint256 freq = userToContractVisitationFreq[tx.origin][msg.sender]++;
+        emit RequestEmited(abi.encode(name));
+        return (tx.origin, freq);
+    }
+
+
+    function emitReview(string calldata review) external {
         emit ReviewEmited(abi.encode((review)));
     }
 
--- src/interfaces/IInbox.sol
+++ src/interfaces/IInbox.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.17;
 
 interface IInbox {
-    function emitRequest(string memory name) external;
-    function emitReview(string memory review) external;
+    function emitRequest(string calldata name) external returns (address, uint256);
+    function emitReview(string calldata review) external;
 }
