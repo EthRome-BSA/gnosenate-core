71f5e78ce326267458d267425a282dd3a53ffe8c
TheoLeFur <theo.lefur@epfl.ch>
2023-10-08 07:00:23 +0200

    Inbox interface

--- src/interfaces/IInbox.sol
+++ src/interfaces/IInbox.sol
@@ -3,13 +3,28 @@ pragma solidity ^0.8.17;
 import {Structs} from "src/libraries/structs.sol";
 
 interface IInbox {
-    function registerContract(address caller) external;
+    /**
+    Writes the contract address,mapped onto its name, inside the Registry
+    @param contractAddress : address of the contract we register. 
+     */
+    function registerContract(address contractAddress) external;
 
+    /**
+    Register the caller inside the private mapping. 
+    @param caller : address of the user. 
+    @param contractAddress : address of the contract he called in the protocol.
+     */
     function registerCall(
         address caller,
         address contractAddress
     ) external returns (address, uint256);
 
+    /**
+    Propagate the review onto IPFS if proof of membership in the group of eligible reviewers is 
+    verified
+    @param review : data containing the review
+    @param response : proof of membership in the Sismo group. 
+     */
     function propagateReview(
         Structs.Review memory review,
         bytes memory response
